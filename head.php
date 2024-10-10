ab0578532d8d26a2da66fffbfda56a11c5f39d74
kelvino04 <kelvinsophie04@gmail.com>
2024-10-10 09:28:27 +0200

    van php naar html

--- head.php
+++
@@ -1,8 +0,0 @@
-<?php require_once 'backend/config.php'; ?>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="fontawesome/css/all.min.css">
-	<title>curio</title>
-</head>
